c46884c4ce511a2cb8d4e97b12b306f060f38450
Dirk Hoffmann <hoffi.mail@gmail.com>
2022-06-16 10:00:22 +0200

    upgraded versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 
 // implemented in buildSrc/src/main/kotlin/Deps.kt
-tasks.register<CheckVersionsTask>("checkVersions") { // implemented in buildSrc/src/main/kotlin/Deps.kt
+tasks.register<CheckVersionsTask>("checkVersions") { // implemented in buildSrc/src/main/kotlin/CheckVersionsTask.kt
     scope = "ALL"
 }
 
